56fbf94f238f3260c8dc9f38a94441965d062fd9
Aurele Karega <a.karega@alustudent.com>
2025-05-31 03:33:51 -0700

    javascript

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
 
-class Square extends SquareP {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint(c) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.width; i++) {
+      console.log(char.repeat(this.width));
     }
   }
 }
