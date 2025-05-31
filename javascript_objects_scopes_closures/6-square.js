a6b14350d531493cf8ae518efbad0af2459eb1df
Aurele Karega <a.karega@alustudent.com>
2025-05-31 03:35:56 -0700

    javascript

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -6,9 +6,9 @@ class Square extends SquareBase {
   charPrint(c) {
     const char = c === undefined ? 'X' : c;
     for (let i = 0; i < this.width; i++) {
-      console.log(char.repeat(this.width));
+      console.log(char.repeat(this.width))
     }
   }
 }
 
-module.exports = Square;
+module.exports = Square
