d6804bf5a7053c5a6e0c21dabe77c784f595bd12
Aurele Karega <a.karega@alustudent.com>
2025-06-07 05:30:41 -0700

    java

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+
+// Read the file using UTF-8 encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
