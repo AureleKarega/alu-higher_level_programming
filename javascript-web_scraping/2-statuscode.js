77d0650cc3a2526af662da927217738df1d96b7b
Aurele Karega <a.karega@alustudent.com>
2025-06-07 05:33:15 -0700

    java

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+// Get the URL from the command line arguments
+const url = process.argv[2];
+
+// Make a GET request
+request(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
+
