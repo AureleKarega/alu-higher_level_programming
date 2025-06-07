edc9296dae3aa3588622443715b9cf17762a33d4
Aurele Karega <a.karega@alustudent.com>
2025-06-07 05:39:32 -0700

    java

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const url = process.argv[2];
+
+// Make GET request
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
+
+
