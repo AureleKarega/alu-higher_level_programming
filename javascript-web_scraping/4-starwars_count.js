420b9635134fa4810a0b58852f620508d1ab5671
Aurele Karega <a.karega@alustudent.com>
2025-06-07 05:35:53 -0700

    java

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const wedgeId = '18';
+
+// Make GET request to the films API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const films = data.results;
+
+  // Count how many films include character ID 18
+  const count = films.filter(film =>
+    film.characters.some(url => url.includes(`/people/${wedgeId}/`))
+  ).length;
+
+  console.log(count);
+});
+
