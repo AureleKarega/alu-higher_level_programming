4fa51a0c3ded362de09dd07f107bf54860bf476b
AureleKarega <a.karega@alustudent.com>
2025-06-07 14:14:14 +0000

    java

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
   if (error) {
@@ -11,41 +11,23 @@ request(url, (error, response, body) => {
     return;
   }
 
-  const film = JSON.parse(body);
-  const characters = film.characters;
+  const characters = JSON.parse(body).characters;
 
-  characters.forEach((charUrl) => {
-    request(charUrl, (err, res, charBody) => {
-      if (!err) {
-        const character = JSON.parse(charBody);
-        console.log(character.name);
+  // Function to fetch and print each character in order
+  const fetchCharacter = (index) => {
+    if (index >= characters.length) return;
+
+    request(characters[index], (err, res, charBody) => {
+      if (err) {
+        console.error(err);
+        return;
       }
+
+      console.log(JSON.parse(charBody).name);
+      fetchCharacter(index + 1);
     });
-  });
-});
-#!/usr/bin/node
+  };
 
-const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  characters.forEach((charUrl) => {
-    request(charUrl, (err, res, charBody) => {
-      if (!err) {
-        const character = JSON.parse(charBody);
-        console.log(character.name);
-      }
-    });
-  });
+  fetchCharacter(0); // start the recursive fetch
 });
 
