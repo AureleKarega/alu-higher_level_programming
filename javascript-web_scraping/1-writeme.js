d03be214c5adad1d2e3b3f660e2d98f6e009114c
Aurele Karega <a.karega@alustudent.com>
2025-06-07 05:31:55 -0700

    java

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// Get arguments from command line
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write to the file using UTF-8 encoding
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
